157c9b34a30f34d3298cb56fae3c7bab2a20e686
Jagoda Giurg <jagoda.giurg@gmail.com>
2022-12-08 16:43:51 +0000

    Remove if from WTForm

--- WTForms/models.py
+++ WTForms/models.py
@@ -1,6 +1,6 @@
 import json
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, HiddenField
 from wtforms.validators import DataRequired
 
 
